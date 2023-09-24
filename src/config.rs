5939244a6edb3c0a37878836b6cb8a6bbd5eafd5
tanner0101 <me@tanner.xyz>
2023-09-23 23:31:38 -0500

    config refactor

---
+++ src/config.rs
@@ -0,0 +1,17 @@
+pub struct Config {
+    pub peer_addr: String,
+    pub port: i32,
+    pub input_device_name: String,
+    pub output_device_name: String,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        return Config {
+            peer_addr: String::from("72.180.248.254"),
+            port: 5060,
+            input_device_name: String::from("sysdefault:CARD=X"),
+            output_device_name: String::from("sysdefault:CARD=X"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
-mod net_util;
+mod config;
 mod poormans;
 mod gpio_util;
 mod rodio_util;
+mod pcm;
 
-use std;
+use std::{self, net};
 use std::io;
 use biquad::ToHertz;
+use config::Config;
 use rodio::{cpal, DeviceTrait};
 use rodio::cpal::traits::{HostTrait, StreamTrait};
 
 fn main() -> io::Result<()> {
-    let ctx = net_util::Context::new("72.180.248.254");
-    let (_device, mic_config, _stream) = start_microphone(&ctx);
-    start_speaker(&ctx, mic_config);
+    let cfg = Config::new();
+
+    // bind to random local port for sending
+    // remote_addr is passed in socket send_to
+    let remote_addr = format!("{}:{}", cfg.peer_addr, cfg.port);
+    let send_socket = net::UdpSocket::bind("0.0.0.0:0")
+        .expect("Failed to bind send socket");
+
+    // bind to configured port for receiving
+    let local_addr = format!("0.0.0.0:{}", cfg.port);
+    let recv_socket = net::UdpSocket::bind(local_addr)
+        .expect("Failed to bind recv socket");
+
+    // start microphone and speaker
+    let (_device, mic_config, _stream) = start_microphone(send_socket, remote_addr);
+    start_speaker(recv_socket, mic_config);
+
     return io::Result::Ok(());
 }
 
-fn start_microphone(ctx: &net_util::Context) -> (rodio::Device, rodio::SupportedStreamConfig, cpal::Stream) {
+fn start_microphone(sock: net::UdpSocket, addr: String) -> (rodio::Device, rodio::SupportedStreamConfig, cpal::Stream) {
     println!("\nstarting microphone...");
     let host = cpal::default_host();
     let device = rodio_util::find_device_by_name(
@@ -25,19 +41,15 @@ fn start_microphone(ctx: &net_util::Context) -> (rodio::Device, rodio::Supported
     ).unwrap();
     let config = device.default_input_config().unwrap();
     let mut filter = poormans::Filter::new(config.sample_rate().0.hz());
-    let send_socket = ctx.send_socket.try_clone().unwrap();
-    let remote_addr = ctx.remote_addr.clone();
     let input_stream = device.build_input_stream(
         &config.clone().into(),
         move |_data: &[i16], _: &cpal::InputCallbackInfo| {
             if !gpio_util::is_call_active() {
                 return;
             }
-            let len = _data.len() * std::mem::size_of::<i16>();
             let data: Vec<i16> = _data.iter().map(|x| filter.run(*x)).collect();
-            let ptr = data.as_ptr() as *const u8;
-            let slice_u8: &[u8] = unsafe { std::slice::from_raw_parts(ptr, len) };
-            send_socket.send_to(slice_u8, &remote_addr).expect("failed to send mic data");
+            sock.send_to(pcm::to_buf(&data), &addr)
+                .expect("failed to send");
         },
         move |err| {
             eprintln!("an error occurred on stream: {}", err);
@@ -53,7 +65,7 @@ fn start_microphone(ctx: &net_util::Context) -> (rodio::Device, rodio::Supported
     return (device, config, input_stream)
 }
 
-fn start_speaker(ctx: &net_util::Context, config: rodio::SupportedStreamConfig) {
+fn start_speaker(sock: net::UdpSocket, mic_config: rodio::SupportedStreamConfig) {
     println!("\nstarting speaker...");
     let device = rodio_util::find_device_by_name(
         cpal::default_host().output_devices().unwrap(),
@@ -62,22 +74,17 @@ fn start_speaker(ctx: &net_util::Context, config: rodio::SupportedStreamConfig) 
     let (_os, output_stream_handle) = rodio::OutputStream::try_from_device(&device).unwrap();
     let sink = rodio::Sink::try_new(&output_stream_handle).unwrap();
     let mut buf = [0u8; 16_384*2];
-    println!("starting loop");
     loop {
-        let (amt, _src) = ctx.recv_socket.recv_from(&mut buf)
-            .expect("Failed to read socket");
+        let (len, _src) = sock.recv_from(&mut buf)
+            .expect("failed to read");
         if !gpio_util::is_call_active() {
             continue;
         }
 
-        let ptr = buf.as_ptr() as *const i16;
-        let len = amt / std::mem::size_of::<i16>();
-        let samples: &[i16] = unsafe { std::slice::from_raw_parts(ptr, len) };
-
         let source = rodio::buffer::SamplesBuffer::new(
-            config.channels(),
-            config.sample_rate().0,
-            samples.clone(),
+            mic_config.channels(),
+            mic_config.sample_rate().0,
+            pcm::from_buf(&buf, len),
         );
         sink.append(source);
     }
