b0c7256af955d7bd74ec99fd31b21e127b720a13
cusps <bunjake@gmail.com>
2023-10-28 12:53:17 -0500

    Finish up call state machine

--- src/call_state_machine.rs
+++ src/call_state_machine.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub enum CallState {
     Idle,
     OutgoingCall,
@@ -5,16 +6,52 @@ pub enum CallState {
     InProgressCall,
 }
 
-pub enum CallSwitchState {
+#[derive(Debug)]
+pub enum CallSwitchEdge {
     NoEdge,
-    Pressed,
-    Released,
+    Active,
+    Inactive,
+}
+
+pub enum CallSwitchState {
+    Active,
+    Inactive,
+}
+
+pub struct CallSwitch {
+    current_state: Option<CallSwitchState>,
+}
+
+impl CallSwitch {
+    fn new() -> Self {
+        CallSwitch {
+            //TODO read GPIO pin? Or set to None?
+            current_state: None,
+        }
+    }
+
+    fn dispatch(mut self, new_switch_state: CallSwitchState) {
+        use CallSwitchState::*;
+
+        let edge: CallSwitchEdge = match (self.current_state, new_switch_state) {
+            /* First call to dispatch */
+            (None, _) => CallSwitchEdge::NoEdge,
+
+            (Some(Inactive), Active) => CallSwitchEdge::Active,
+            (Some(Active), Inactive) => CallSwitchEdge::Inactive,
+            (Some(Inactive), Inactive) => CallSwitchEdge::NoEdge,
+            (Some(Active), Active) => CallSwitchEdge::NoEdge,
+        };
+
+        self.current_state = Some(new_switch_state);
+    }
 }
 
 pub struct Call {
     state: CallState,
 }
 
+#[derive(Debug)]
 pub enum PacketType {
     Ring,
     RingAck,
@@ -28,30 +65,67 @@ impl Call {
         }
     }
 
-    fn dispatch(mut self, header: Option<PacketType>, call_switch: CallSwitchState) {
+    fn dispatch(mut self, packet_type: Option<PacketType>, call_switch_edge: CallSwitchEdge) {
         use CallState::*;
-        use CallSwitchState::*;
+        use CallSwitchEdge::*;
         use PacketType::*;
-        let mut outgoing_call_time = 0;
-        self.state = match (self.state, header, call_switch) {
+        self.state = match (self.state, packet_type, call_switch_edge) {
+            /* First we walk through nominal path. */
+
+            /* Generally nothing happening */
             (Idle, None, NoEdge) => CallState::Idle,
-            (OutgoingCall | InProgressCall, _, Pressed) => Idle,
 
-            (Idle, Some(Ring), NoEdge | Pressed) => IncomingCall,
+            /* Switch activated, call other phone. */
+            (Idle, None, Active) => OutgoingCall,
 
-            (OutgoingCall, Some(RingAck) | None, NoEdge) => {
-                outgoing_call_time += 1;
+            /* Calling other phone - TODO implement timeout eventually */
+            (OutgoingCall, Some(RingAck) | None, NoEdge) => OutgoingCall,
 
-                match outgoing_call_time > 10 {
-                    false => OutgoingCall,
-                    true => Idle,
-                }
+            /* Once voice data is received, call has started. */
+            (OutgoingCall, Some(VoiceData), NoEdge) => InProgressCall,
+
+            /* Start getting called by the other phone. */
+            (Idle, Some(Ring), NoEdge) => IncomingCall,
+
+            /* Still being rung - TODO add timeouts */
+            (IncomingCall, Some(Ring) | None, NoEdge) => IncomingCall,
+
+            /* Answering incoming call. */
+            (IncomingCall, Some(Ring) | None, Active) => InProgressCall,
+
+            /* Ongoing call - TODO implement timeout for None */
+            (InProgressCall, Some(VoiceData) | None, NoEdge) => InProgressCall,
+
+            /* Hangup */
+            (OutgoingCall | InProgressCall, _, Inactive) => Idle,
+
+            /* Somehow we called each other at the same time, just answer. */
+            (OutgoingCall, Some(Ring), NoEdge) => InProgressCall,
+            (Idle, Some(Ring), Active) => InProgressCall,
+
+            /* These cases are nominal edge cases (when moving from states). */
+            (Idle, Some(RingAck), _) => Idle,
+            (Idle, Some(VoiceData), _) => Idle,
+            (InProgressCall, Some(Ring), NoEdge) => InProgressCall,
+
+            /* These states shouldn't happen. Error and reset. */
+            (Idle, None | Some(Ring), Inactive)
+            | (IncomingCall, Some(Ring), Inactive)
+            | (IncomingCall, Some(RingAck) | Some(VoiceData), _)
+            | (IncomingCall, None, Inactive)
+            | (OutgoingCall, _, Active)
+            | (InProgressCall, Some(RingAck), NoEdge)
+            | (InProgressCall, _, Active) => {
+                println!(
+                    "\nERROR: CallState state machine: \n\
+                    CallState: {:?}\n\
+                    Packet Type: {:?}\n\
+                    Call Switch: {:?}\n",
+                    self.state, packet_type, call_switch_edge
+                );
+
+                Idle
             }
-            (Idle, None, Released) => OutgoingCall,
-
-            (Idle, Some(Ring), Released) => InProgressCall,
-            (IncomingCall, Some(Ring) | None, Released) => InProgressCall,
-            // (Idle, None, CallSwitchState::Pressed) => CallState::Idle,
         }
     }
 }
